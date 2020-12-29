6a2c1003f6a5bae49d89ce7f840b0fcb81085842
Aleksey Kliger <alklig@microsoft.com>
2020-12-29 16:52:02 -0500

    setup bare minimum native code to init mono

--- src/native/main.c
+++ src/native/main.c
@@ -12,8 +12,9 @@ mono_jit_init_version      (const char *root_domain_name, const char *runtime_ve
 int
 main (void)
 {
-	const char *prop_keys[1] = {NULL};
-	const char *prop_values[1] = {NULL};
-	monovm_initialize (0, (const char**) &prop_keys, (const char**) &prop_values);
+	const char *prop_keys[] = {"TRUSTED_PLATFORM_ASSEMBLIES"};
+	char *prop_values[] = {CORELIB_FULL_PATH};
+	int nprops = sizeof(prop_keys)/sizeof(prop_keys[0]);
+	monovm_initialize (nprops, (const char**) &prop_keys, (const char**) &prop_values);
 	MonoDomain *root_domain = mono_jit_init_version ("embedder_sample", "v4.0.30319");
 }
